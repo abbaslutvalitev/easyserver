fffc4b9f4174de8872a5612d0b0f22e30b2f088f
Abbas Lutvaliyev <91781891+abbaslutvaliyev@users.noreply.github.com>
2022-03-09 09:56:55 +0400

    Update easyserver.py

--- easyserver.py
+++ easyserver.py
@@ -1,11 +1,14 @@
-#Pyserver source code
-from http import server
-from http.server import HTTPServer, CGIHTTPRequestHandler
-ip = input("Enter the server ip! For example: localhost: ")
-port = int(input("Enter the port: "))
-serverdata = (ip, port)
+#easyserver source code
+from http import server#import 1 
+from http.server import HTTPServer, CGIHTTPRequestHandler#import 2
+ip = input("Enter the server ip! For example: localhost: ")#ip
+port = int(input("Enter the port: "))#port
+serverdata = (ip, port)#creating tuple with server ip and port
 
-server = HTTPServer(serverdata, CGIHTTPRequestHandler)
-print("Server runned at: " + ip + ":" + str(port))
-server.serve_forever()
+server = HTTPServer(serverdata, CGIHTTPRequestHandler)#creating server
+print("Server runned at: " + ip + ":" + str(port))#run info
+server.serve_forever()#running
+#Version: 1.0
+#Author: Abbas Lutvaliyev
+#Telegram: @alutvaliyev
 
